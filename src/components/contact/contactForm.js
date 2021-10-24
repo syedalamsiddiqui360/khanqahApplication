d1dedb3c1a1d73b6b58a39a689a77f3fcb7d7c1e
syedfaizan2021 <mega360solutions@gmail.com>
2021-10-24 19:15:31 +0500

    Not Done yed

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -6,17 +6,28 @@ function ContactForm() {
     const [isDisable, setIsDisable] = useState(true);
     const [isUpdate, setIsUpdate] = useState(false);
     const [validEmail , setValidEmail] = useState(false);
-    const [message , setMessage] = useState('sdsds');
+    const [message , setMessage] = useState('');
     const [data, setData] = useState({
         'name': '',
         'email': '',
+        'gender_id': '0',
         'islahiTaluq': '',
         'question': ''
     });
+    const [genderList, setGenderList] = useState([
+        {
+            'id': 1,
+            'title': "Male",
+        },
+        {
+            'id': 2,
+            'title': "Female",
+        }
+    ]);
     const reg = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g);
 
     useEffect(()=>{
-        if(data.name !== "" && reg.test(data.email) && data.phoneNumber !== "" ){
+        if(data.name !== "" && reg.test(data.email) && data.gender_id !== '0' && data.islahiTaluq !== "" && data.question !== "" ){
             setIsDisable(false)
         }
         else{
@@ -34,15 +45,22 @@ function ContactForm() {
         setIsUpdate(!isUpdate);
     }
 
-    const handleNumber = (e) => {
-        data.phoneNumber = e.target.value;
+    const handleGender = (e) => {
+        data.gender_id = e.target.value;
+        setIsUpdate(!isUpdate);
+    }
+
+    const handleIslahiTaluq = (e) => {
+        data.islahiTaluq = e.target.value;
+        setIsUpdate(!isUpdate);
+    }
+
+    const handleQuestion = (e) => {
+        data.question = e.target.value;
         setIsUpdate(!isUpdate);
     }
 
     const submit = () => {
-        const reg = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g);
-        if(reg.test(data.email)){
-            setValidEmail(false);
         axios.post('http://localhost:9000/follower/add_whatsapp_no', data)
         .then((res) => {
             // data = res.data;
@@ -52,10 +70,6 @@ function ContactForm() {
             setMessage("Something went wrong")
             console.log('FAILURE!!' + err);
         });
-    }
-    else{
-        setValidEmail(true);
-    }
     }
 
     return (
@@ -75,14 +89,27 @@ function ContactForm() {
                 </div>
                 <div class="row form-group">
                     <div class="col-md-12">
+                        <label class="font-weight-bold" for="email">Gender</label>
+                        <select class="form-control" onChange={(e) => handleGender(e)}>
+                            <option value="0">Select Gender</option>
+                            {
+                                genderList.map(element => {
+                                    return <option value={element.id}>{element.title}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-md-12">
                         <label class="font-weight-bold" for="">Current Islahi talluk</label>
-                        <input type="text" required onChange={(e) => handleNumber(e)} class="form-control" placeholder="Islahi talluk" />
+                        <input type="text" required onChange={(e) => handleIslahiTaluq(e)} class="form-control" placeholder="Islahi talluk" />
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-md-12">
                         <label class="font-weight-bold" for="">Question</label>
-                        <input type="text" required onChange={(e) => handleNumber(e)} class="form-control" placeholder="Question" />
+                        <input type="text" required onChange={(e) => handleQuestion(e)} class="form-control" placeholder="Question" />
                     </div>
                 </div>
                 {message !== '' && (
